e9e27b34e03f9eaea310bf5b76f98e14f44fb838
ZImmerman15 <59876296+ZImmerman15@users.noreply.github.com>
2020-02-18 09:18:14 -0500

    IT_242_1 changes
    Updates

---
+++ HelloWorld3/HelloWorld3/HelloWorld3/bin/Types/Types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        private static object firstName;
+
+        static void Main(string[] args)
+        {
+            string firstName = "Zach";
+            int grade = 98;
+            char letterGrade = 'A';
+                double costOfTuition = 15000.00;
+
+            Console.WriteLine($"{firstName} is the proffesor, {grade} is the grade earned. which is an{letterGrade} and it cost {costOfTuition} to attend his class");
+            Console.ReadLine();
+        }
+    }
+}
